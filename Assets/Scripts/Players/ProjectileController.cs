d6bcf268bed08a343a4ddfe8de5cf52d3efc726e
Adria Abella <adria.abella@visyon360.com>
2020-11-13 23:39:35 +0100

    Added projectiles.

---
+++ Assets/Scripts/Players/ProjectileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    private Vector3 _moveDirection = Vector3.zero;
+    public bool alive = true;
+
+    void Start()
+    {
+        _moveDirection.y = 15.0f;
+    }
+
+    void Update()
+    {
+        transform.Translate(_moveDirection * Time.deltaTime, Space.World);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ceiling")
+        {
+            alive = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
