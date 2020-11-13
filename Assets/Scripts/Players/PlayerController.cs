69fa6e5409aef3595977073e88be1d16e37a630e
Sergi Tortosa <tortosa95@gmail.com>
2020-11-13 21:41:58 +0100

    Base for suicidal

---
+++ Assets/Scripts/Players/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // A player can:
+    //  Move wasd arrows etc
+    //  Attack something
+    //  Block stun
+    //  A player always faces the other player bc you know fgc
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
