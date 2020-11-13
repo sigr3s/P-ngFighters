537891a5741c17c49c9789d1b9db5a08aae7454f
Sergi Tortosa <tortosa95@gmail.com>
2020-11-13 22:17:38 +0100

    Player input actions

--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    private PlayerInput _playerInput;
+    private PlayerInput m_PlayerInput {
+        get{
+            if(_playerInput){
+                _playerInput = GetComponent<PlayerInput>();
+            }
+
+            return _playerInput;
+        }
+    }
+    private InputAction m_MoveAction {
+        get{
+           return m_PlayerInput.actions["move"];
+        }
+    }
+
+    private InputAction m_JumpAction {
+        get{
+           return m_PlayerInput.actions["jump"];
+        }
+    }
+
+    private InputAction m_FireAction{
+        get{
+            return m_PlayerInput.actions["fire"];
+        }
+    }
 
     // A player can:
     //  Move wasd arrows etc
     //  Attack something
     //  Block stun
     //  A player always faces the other player bc you know fgc
-    
+
 
     void Start()
     {
@@ -19,6 +48,10 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        if (m_FireAction.triggered){
+             
+        }
+
+        var move = m_MoveAction.ReadValue<Vector2>();
     }
 }
