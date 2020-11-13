f838c8ef91b44c923dde86c3f05cc8f64b7f8da2
Adria Abella <adria.abella@visyon360.com>
2020-11-13 22:24:18 +0100

    asdasfafasdasf

--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -1,24 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-
     // A player can:
     //  Move wasd arrows etc
     //  Attack something
     //  Block stun
     //  A player always faces the other player bc you know fgc
+
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController _charaterController = null;
     
+    void Awake()
+    {
+        _charaterController = GetComponent<CharacterController>();
+    }
 
     void Start()
     {
-        
+        moveDirection.y = jumpSpeed;
     }
 
     void Update()
     {
-        
+        if (_charaterController.isGrounded) {
+            moveDirection.y = jumpSpeed;
+        }
+        if (.triggered){
+
+        }
+
+        var move = m_MoveAction.ReadValue<Vector2>();
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        _charaterController.Move(moveDirection * Time.deltaTime);
     }
 }
