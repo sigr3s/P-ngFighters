5736f81816e6db200444d66575872a37f0a772c0
Sergi Tortosa <tortosa95@gmail.com>
2020-11-14 23:41:58 +0100

    Master?

--- Assets/Scripts/Gameplay/Hazard.cs
+++ Assets/Scripts/Gameplay/Hazard.cs
@@ -115,7 +115,7 @@ public class Hazard : MonoBehaviour
 
         if(DataUtility.gameData.isNetworkedGame){
             Debug.Log("Call this?");
-            PunTools.PhotonRpcMine(view, "RPC_DestroyHazard", RpcTarget.AllBuffered, player);
+            PunTools.PhotonRpcMineAndMaster(view, "RPC_DestroyHazard", RpcTarget.AllBuffered, player);
             return true;
         }   
         else{
