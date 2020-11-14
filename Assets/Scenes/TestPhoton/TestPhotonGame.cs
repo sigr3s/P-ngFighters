8c50c297e5110ff4bbf3e6150594dc78895411fb
AlexHigon <alex.higon@visyon360.com>
2020-11-14 14:43:11 +0100

    Updated lobby and added text mesh pro

---
+++ Assets/Scenes/TestPhoton/TestPhotonGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Photon.Pun;
+using UnityEngine;
+
+public class TestPhotonGame : MonoBehaviour
+{
+    string pathRelativeToResources = "PhotonPrefabs";
+    string prefabName => "PhotonDummyPlayer";
+
+    void Start() 
+    {        
+        PhotonNetwork.Instantiate(Path.Combine(pathRelativeToResources, prefabName), Vector3.zero, Quaternion.identity);
+    }
+}
